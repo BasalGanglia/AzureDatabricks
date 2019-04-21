bec0505ff96642b2469d2a49c4b0435d24181c7e
BasalGanglia <BasalGanglia@users.noreply.github.com>
2019-04-21 11:06:18 +0300

    finished for today on this part

--- notebooks/Users/ilkka.kosunen@gmail.com/pytorch60min/faciallandmarks.py
+++ notebooks/Users/ilkka.kosunen@gmail.com/pytorch60min/faciallandmarks.py
@@ -1,1 +1,24 @@
 # Databricks notebook source
+from __future__ import print_function, division
+import os
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+plt.ion()   # interactive mode
+
+# COMMAND ----------
+
+have to import the data (later)
+#landmarks_frame = pd.read_csv('data/faces/face_landmarks.csv')
+
+# COMMAND ----------
+
